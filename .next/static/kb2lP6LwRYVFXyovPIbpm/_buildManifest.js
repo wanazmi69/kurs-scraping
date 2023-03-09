6546ed2416c59cac0928ad4732283a60e8ae5eae
edihasrin <edyhasryn7@gmail.com>
2023-03-10 00:13:46 +0800

    tes lagi

---
+++ .next/static/kb2lP6LwRYVFXyovPIbpm/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b006b331397fd480.css","static/chunks/pages/index-bf26b92211a9a2d0.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
