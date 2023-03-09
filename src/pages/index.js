571986ef698125bf17b96de5784c0e30f5ee0c75
edihasrin <edyhasryn7@gmail.com>
2023-03-09 23:53:16 +0800

    tes1

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<main className={styles.main}>
-				<p>Ambil Kurs</p>
+				<p>Ambil Kurs Mata Uang</p>
 			</main>
 		</>
 	);
